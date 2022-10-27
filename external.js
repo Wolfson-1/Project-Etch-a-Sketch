e4edf837c756f5c42fb1cb4644fdd70f8b09de36
wolfson-1 <harrywolverson@googlemail.com>
2022-10-27 22:54:03 +0100

    CREATE RESIZE BUTTON & START TO SETUP JS CODE: Setup resize button & appropriate js code to resize sketch grid. JS code not yet complete.

--- external.js
+++ external.js
@@ -1,9 +1,14 @@
+//variables
+let gridSize = 256;
+
 // Query selectors for main div container & resetn button
 const sketchContainer = document.querySelector(".sketchContainer");
 const resetButton = document.querySelector(".reset");
+const sizeButton = document.querySelector(".size");
 
 // for loop to create div grid & apply background color change effect
-for (let x = 0; x < 256; x++) {
+
+for (let x = 0; x < gridSize; x++) {
     //creation of div grid
    const sketchDivCol = document.createElement("div");
    sketchDivCol.classList.add("sketchDivCol");
@@ -20,6 +25,30 @@ resetButton.addEventListener('click', () => {
 })
 
 };
+
+// function to change grid size 
+function gridSizeChange(size){
+    sketchContainer.style.gridTemplateColumns = repeat(size, 1fr);
+    sketchContainer.style.gridTemplateRows = repeat(size, 1fr);
+};
+
+//event listener for  resize of grid
+sizeButton.addEventListener('click', () => {
+    
+    gridSize = prompt("Enter grid size", "0");
+
+    if (gridSize >= 0) {
+        alert("Thanks!");
+        gridSizeChange(gridSize);
+    } else {
+        alert("Not a number, please try again")
+        gridSize = 256;
+    }
+});
+
+
+
+
 
 
 
